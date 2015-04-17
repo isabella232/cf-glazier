f5100e061f153570b97fd4ac2ae3768bea9f29d4
Calin Miclaus <calin.miclaus@hp.com>
2015-04-17 18:59:39 +0300

    Add OS_CACERT support, add helper functions, call them from shell.ps1

--- assets/shell.ps1
+++ assets/shell.ps1
@@ -1,1 +1,9 @@
+Import-Module A:\common\openstack-tools.psm1
+
+Verify-PythonClientsInstallation
+Check-OpenRCEnvVars
+#Check-ArgsList
+Validate-WindowsISO
+Check-CACERT
+
 echo "Welcome to glazier"
